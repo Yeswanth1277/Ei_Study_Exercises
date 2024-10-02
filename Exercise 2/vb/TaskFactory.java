4f7b95e29bde9de26d4be9147f9124acfcd08892
Yeswanth1277 <99649327+Yeswanth1277@users.noreply.github.com>
2024-10-02 16:02:08 +0530

    Delete Exercise 2/vb directory

--- Exercise 2/vb/TaskFactory.java
+++
@@ -1,7 +0,0 @@
-package question1;
-import java.time.LocalTime;
-public class TaskFactory {
-    public static Task createTask(String description, LocalTime startTime, LocalTime endTime, String priority) {
-        return new Task(description, startTime, endTime, priority);
-    }
-}
